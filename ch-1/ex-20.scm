a78c2ff9951f81e66bac4b76dbc4eed90277e8f7
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2022-01-24 22:42:45 +0100

    1.21-24

--- ch-1/ex-20.scm
+++ ch-1/ex-20.scm
@@ -1,4 +1,4 @@
-; Exercise 1.20.  The process that a procedure generates is of course dependent
+; Exercise 1.20. The process that a procedure generates is of course dependent
 ; on the rules used by the interpreter. As an example, consider the iterative
 ; gcd procedure given above. Suppose we were to interpret this procedure using
 ; normal-order evaluation, as discussed in section 1.1.5. (The
